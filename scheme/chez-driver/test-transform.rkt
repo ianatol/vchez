e641db80dd3c65aee8882952e62fa64c606ff635
ianatol <ian.r.atol@gmail.com>
2021-05-15 13:58:41 -0700

    Modify r6rs semantics for eval order, set! evaluation result

--- scheme/chez-driver/test-transform.rkt
+++ scheme/chez-driver/test-transform.rkt
@@ -2,7 +2,7 @@
 
 
 (require redex
-         redex/examples/r6rs/r6rs
+         "../rvrs.rkt"
          "../transform.rkt")
 
 ;(traces reductions (term (store () (letrec ([x 14]) (begin (set! x 15) x)))))
@@ -12,10 +12,15 @@
  (term
   (store () (letrec ([x 14]) (begin (set! x 15) x)))))
 
-(apply-reduction-relation
+#;(apply-reduction-relation
  reductions
  (term
   (store () ((lambda (x) x) 3))))
+
+(apply-reduction-relation
+ reductions
+ '(store () ((lambda (x) x) 3)))
+
 
 
 
