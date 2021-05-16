9a9de4b06ece0978ceeb190349132ee74a55aa8f
ianatol <ian.r.atol@gmail.com>
2021-05-16 14:06:29 -0700

    Minor cleanup

--- scheme/chez-driver/test-transform.rkt
+++ scheme/chez-driver/test-transform.rkt
@@ -1,13 +1,9 @@
 #lang racket
 
 (require redex
-         ;redex/examples/r6rs/r6rs
          rackunit
-         ;"../rvrs.rkt"
          "../rvrs2.rkt"
          "../transform.rkt")
-
-;(traces reductions (term (store () (letrec ([x 14]) (begin (set! x 15) x)))))
 
 (define (step prog)
   (car (apply-reduction-relation
@@ -35,6 +31,18 @@
    prog
    #:all? #t))
 
+(define (show-Ps prog)
+  (displayln 'P)
+  (displayln prog)
+  (displayln 'P~)
+  (displayln (step prog))
+  (displayln 'P^)
+  (displayln (ca/prog prog))
+  (displayln 'P~^)
+  (displayln (ca/prog (step prog)))
+  (displayln 'P^~*)
+  (display (step* (ca/prog prog))))
+
 (define (test-ca-single prog)
   (check-equal?
    (ca/prog (step prog))
@@ -49,19 +57,9 @@
     (member P~^ (step* P^)))
    #f))
 
-(define (show-Ps prog)
-  (displayln 'P)
-  (displayln prog)
-  (displayln 'P~)
-  (displayln (step prog))
-  (displayln 'P^)
-  (displayln (ca/prog prog))
-  (displayln 'P~^)
-  (displayln (ca/prog (step prog)))
-  (displayln 'P^~*)
-  (display (step* (ca/prog prog))))
-
-;(show-Ps '(store () ((lambda (x) (set! x 5)) 4)))
+(test-ca-single '(store () (+ 3 4)))
+(test-ca-many '(store ((x 5)) (set! x 4)))
+(test-ca-single '(store ((x 5)) (set! x 4)))
 
 ;(test-ca-many '(store () ((lambda (x) (set! x 5)) 4)))
 ;this should work but doesn't due to names not matching
@@ -69,28 +67,6 @@
 ;P^ = (store () ((lambda (t) ((lambda (x) (set-car! x 5)) (cons t null))) 4))
 ;P~^ = (store (((-mp bp) (cons 4 null))) ((lambda () (set-car! (-mp bp) 5))))
 ;P^~* contains (store (((-mp x1) (cons 4 null))) ((lambda () (set-car! (-mp x1) 5))))
-
-;(show-Ps '(store () (+ 3 4)))
-;(show-Ps '(store ((x 5)) (set! x 4)))
-;(show-Ps '(store () ((lambda (x) (lambda (y) (set! x 3))) 5)))
-#;(let ([prog (ca/prog '(store () ((lambda (x) (set! x 5)) 4)))])
-  (displayln prog)
-  (displayln (step prog))
-  (displayln (step (step prog)))
-  (displayln (step (step (step prog))))
-  (displayln (step (step (step (step prog)))))
-  (displayln (step (step (step (step (step prog)))))))
-
-;(car (apply-reduction-relation/tag-with-names reductions '(store () ((lambda (x) (set! x 5)) 4))))
-;(apply-reduction-relation/tag-with-names reductions '(store () ((lambda (x) (set! x 5)) 4)))
-
-;(step '(store ((x 5)) (begin (null (set! x 3)))))
-;(step '(store (((-mp x) (cons 5 null))) (set-car! (-mp x) 4)))
-(test-ca-single '(store () (+ 3 4)))
-(test-ca-many '(store ((x 5)) (set! x 4)))
-(test-ca-single '(store ((x 5)) (set! x 4)))
-;(test-ca-single '(store () ((lambda (x) x) 3)))
-;(test-ca-many '(store () ((lambda (x) (set! x 5)) 4)))
 
 ;Generates an example of ca being a simulation relation
 ; n is the number of steps that a transformed program takes to get to the program resulting from a single step and then transforming
